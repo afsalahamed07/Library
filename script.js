6e4bd45e8a9e987df04da62294e1f64bae601710
ARAA <afsalahamed07@gmail.com>
2021-05-21 13:48:50 +0530

    working local storage

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let myLibrary = [];
-
+let sessionLibrary = [];
 let numInstances = 1;
+
+// local storage
+let localLibrary = localStorage.getItem('localLibrary') ? JSON.parse(localStorage.getItem('localLibrary')) : [] // let numInstances = localStorage.getItem('numInstances') ? parseInt(localStorage.getItem('numInstances')) : 1;
+    // let numInstances = localStorage.getItem('numInstances') ? parseInt(localStorage.getItem('numInstances')) : 1;
 
 function Book(title, author, pages, read) {
     this.bookId = "book" + numInstances;
@@ -13,31 +16,34 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.addBookToLibrary = function() {
-    myLibrary.push(this);
+    sessionLibrary.push(this);
 };
 
 
 Book.prototype.makeCard = function() {
     return `<div class="col s12 m4" id="${this.bookId}">
-    <div class="card orange lighten-3">
+    <div class="card">
       <div class="card-content">
         <span class="card-title">${this.title}</span>
         <span class="card-title">${this.author}</span>
         <span class="card-title">${this.pages}</span>
         <span class="card-title">${this.read}</span>
       </div>
-      <div class="card-action grey darken-4">
-        <button class="btn-remove" id="btn-${this.bookId}">Remove</button>
-        <button>Read</button>
+      <div class="card-action">
+        <button class="btn btn-remove" id="btn-${this.bookId}">Remove</button>
+        <button class ="btn">Read</button>
       </div>
     </div>
   </div>`
 };
 
 
+
 function removeFromLibrary(bookId) {
-    myLibrary = myLibrary.filter(book => book.bookId != bookId);
+    sessionLibrary = sessionLibrary.filter(book => book.bookId != bookId);
 }
+
+
 
 const books = document.querySelector("#books");
 const bookRow = books.querySelector(".row")
@@ -52,8 +58,6 @@ const bookAuthour = document.querySelector("#book-author");
 const bookPages = document.querySelector("#book-pages");
 const bookRead = document.querySelector("#book-read");
 
-// let btnRemove
-
 
 btnAddBook.addEventListener("click", () => {
     formAddBook.style.display = "block";
@@ -65,6 +69,33 @@ window.onclick = function(event) {
         formAddBook.style.display = "none";
     }
 }
+
+// coveting objects from loaclLibarary to Book objects
+localLibrary.forEach(book => {
+    let read = book.read == "Read" ? true : false;
+    book = new Book(book.title, book.author, book.pages, read)
+    const bookCard = book.makeCard();
+    bookRow.innerHTML += bookCard;
+
+    sessionLibrary.push(book);
+});
+
+let btnRemove = document.querySelectorAll(".btn-remove")
+
+btnRemove.forEach(btn => {
+    btn.addEventListener("click", function() {
+        const bookId = btn.id.substr(4)
+        const domBookCard = document.querySelector(`#${bookId}`)
+
+        removeFromLibrary(bookId);
+        localLibrary = localLibrary.filter(book => book.bookId != bookId);
+
+        localStorage.setItem('localLibrary', JSON.stringify(localLibrary))
+
+        domBookCard.remove();
+    })
+});
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -95,5 +126,8 @@ form.addEventListener("submit", (e) => {
         })
     });
 
-
+    localStorage.setItem('localLibrary', JSON.stringify(sessionLibrary));
+    // localStorage.setItem('numInstances', numInstances)
 });
+
+console.log("hello from the outside")
